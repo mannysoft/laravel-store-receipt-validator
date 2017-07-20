3660a53f2e2f5ce1e0fac16053f6e24a9c63ecb5
Manny Isles <manny.isles@gmail.com>
2017-07-21 01:26:59 +0800

    Create ServiceProvider.php

---
+++ src/Mannysoft/ReceiptValidator/ServiceProvider.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace Mannysoft\ReceiptValidator;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider {
+    
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/receiptvalidator.php' => config_path('receiptvalidator.php'),
+        ]);
+        $this->mergeConfigFrom(
+            __DIR__ . '/../../config/receiptvalidator.php', 'vatlayer'
+        );
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //$this->app->bind('vat-layer', function() {
+            //$apiKey = config('vatlayer.api_key');
+            //return new VatLayer();
+        //});
+    }
+
+}
