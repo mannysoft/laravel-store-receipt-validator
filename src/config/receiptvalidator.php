8c6bd7f873594e2f51513d483a2d397045dbc311
Manny Isles <manny.isles@gmail.com>
2017-07-21 01:46:42 +0800

    validator

---
+++ src/config/receiptvalidator.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    
+    'itunes_endpoint' => env('ITUNES_ENDPOINT', 'sandbox'),
+    
+    
+    
+    
+    'google_play_endpoint' => env('GOOGLE_PLAY_ENDPOINT', 'sandbox'),
+];
